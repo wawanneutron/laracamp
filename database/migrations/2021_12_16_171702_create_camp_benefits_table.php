ee078df276f5a8ceab19185a4ac4feaab0da2ec7
wawanneutron <wawanneutron1331@gmail.com>
2021-12-17 01:03:01 +0700

    first commit (create migrations and models)

---
+++ database/migrations/2021_12_16_171702_create_camp_benefits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            // $table->unsignedBigInteger('camp_id');
+            $table->string('name');
+            $table->foreignId('camp_id')->constrained()
+                ->onDelete('cascade'); //versi pendek syarat nama tabel harus sama
+            $table->timestamps();
+            $table->softDeletes();
+
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade'); versi panjang
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
