5b1d8fd3dc711323892ad80d18e7b2d4f74fe522
wawanneutron <wawanneutron1331@gmail.com>
2021-12-17 21:15:27 +0700

    rename models and seed camp benefit and camp table seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use App\Models\Camps;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug'  => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug'  => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s')
+            ]
+        ];
+        Camp::insert($camps);
+        /* 1st method */
+        // foreach ($camps as $camp) {
+        //     Camp::create($camp);
+        // }
+    }
+}
