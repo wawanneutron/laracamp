e05fda104fbca2f4f04c2f55cb7868de29e54ef6
wawanneutron <wawanneutron1331@gmail.com>
2021-12-22 04:07:16 +0700

    configure laravel breeze

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,2 +1,1 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
- <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,7 +1,6 @@
 <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/assets/styles/main.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
