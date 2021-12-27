9c29d687dec1ee01b1ce04622f4b65c8bcaa763c
wawanneutron <wawanneutron1331@gmail.com>
2021-12-27 21:10:01 +0700

    update checkout page

--- app/Http/Requests/user/checkout/StoreRequest.php
+++ app/Http/Requests/user/checkout/StoreRequest.php
@@ -24,14 +24,16 @@ class StoreRequest extends FormRequest
      */
     public function rules()
     {
-        $expiredValidation = date('Y-m', time());
+        // $expiredValidation = date('Y-m', time());
         return [
             'name'  =>  'required|string',
             'email' =>  'required|email|unique:users,email,' . Auth::id() . ',id',
             'occupation' => 'required|string|between:3,100',
-            'card_number' => 'required|numeric|digits_between:8,16',
-            'expired' => 'required|date|date_format:Y-m|after_or_equal:' . $expiredValidation,
-            'cvc' => 'required|numeric|digits:3'
+            'phone'     => 'required|numeric',
+            'address'   => 'required|string'
+            // 'card_number' => 'required|numeric|digits_between:8,16',
+            // 'expired' => 'required|date|date_format:Y-m|after_or_equal:' . $expiredValidation,
+            // 'cvc' => 'required|numeric|digits:3'
         ];
     }
 }
